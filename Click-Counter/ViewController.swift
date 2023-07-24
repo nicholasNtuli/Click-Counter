61e60aa87d3b04a0982db68c73d3adadb1e09363
Sihle Ntuli <sihle.ntuli@glucode.com>
2023-07-24 20:40:00 +0200

    Experimental view

--- Click-Counter/ViewController.swift
+++ Click-Counter/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
     
     var count = 0
     var lable: UILabel!
+    var secondLable: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,18 +22,38 @@ class ViewController: UIViewController {
         view.addSubview(label)
         self.lable = label
         
+        let secondLable = UILabel()
+        secondLable.frame = CGRect(x: 150, y: 175, width: 60, height: 60)
+        secondLable.text = "0"
+        view.addSubview(secondLable)
+        self.secondLable = secondLable
+        
         let button = UIButton()
         button.frame = CGRect(x: 150, y: 250, width: 60, height: 60)
         button.setTitle("Click", for: .normal)
         button.setTitleColor(.blue, for: .normal)
         view.addSubview(button)
         
+        let secondbutton = UIButton()
+        secondbutton.frame = CGRect(x: 150, y: 350, width: 60, height: 60)
+        secondbutton.setTitle("Click", for: .normal)
+        secondbutton.setTitleColor(.blue, for: .normal)
+        view.addSubview(secondbutton)
+        
         button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControl.Event.touchUpInside)
+        secondbutton.addTarget(self, action: #selector(ViewController.decrementCount), for: UIControl.Event.touchUpInside)
     }
     
     @objc func incrementCount() {
         self.count += 1
         self.lable.text = "\(self.count)"
+        self.secondLable.text = "\(self.count)"
+    }
+    
+    @objc func decrementCount() {
+        self.count -= 1
+        self.lable.text = "\(self.count)"
+        self.secondLable.text = "\(self.count)"
     }
 
 }
